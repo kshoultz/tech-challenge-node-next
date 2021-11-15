638679bdee75ba2e9444d12bb672db7dd340ef20
Kevin <jedi.kevo@gmail.com>
2021-11-15 07:42:53 -0600

    wire up contact form.

--- pages/components/ContactForm.js
+++ pages/components/ContactForm.js
@@ -1,16 +1,43 @@
-function ContactForm() {
+import { useRef } from 'react';
+
+function ContactForm(props) {
+
+    const firstNameInputRef = useRef(),
+        lastNameInputRef = useRef(),
+        titleInputRef = useRef(),
+        emailInputRef = useRef(),
+        messageInputRef = useRef();
+
+        function submitHandler(event) {
+            event.preventDefault();
+            const enteredFirstName = firstNameInputRef.current.value,
+                enteredLastName = lastNameInputRef.current.value,
+                enteredTitle = titleInputRef.current.value,
+                enteredEmail = emailInputRef.current.value,
+                enteredMessage = messageInputRef.current.value;
+
+            const contactFormData = {
+                'first_name': enteredFirstName,
+                'last_name': enteredLastName,
+                'title': enteredTitle,
+                'email': enteredEmail,
+                'message': enteredMessage
+            };
+            
+            props.onAddContact(contactFormData);
+        }
     return (
-        <form className="mwi-form" action="https://api.mwi.dev/contact" method="POST">
+        <form className="mwi-form" onSubmit={submitHandler} >
             <h2>Heading Two</h2>
             <div className="mwi-fields-container">
-                <input placeholder="First Name" name="first_name" />
-                <input placeholder="Last Name" name="last_name" />
-                <input placeholder="Title" name="title" />
-                <input placeholder="Email" name="email" required="required" />
-                <textarea placeholder="Message" name="message" rows="4"></textarea>
+                <input placeholder="First Name" name="first_name" ref={firstNameInputRef} />
+                <input placeholder="Last Name" name="last_name" ref={lastNameInputRef} />
+                <input placeholder="Title" name="title" ref={titleInputRef} />
+                <input placeholder="Email" name="email" required="required" ref={emailInputRef} />
+                <textarea placeholder="Message" name="message" rows="4" ref={messageInputRef}></textarea>
             </div>
             <div className="mwi-toolbar">
-                <input type="submit" value="submit" />
+                <input type="submit" value="submit"  />
             </div>
         </form>
     );
--- pages/contact.js
+++ pages/contact.js
@@ -1,44 +1,55 @@
 import ContactForm from './components/ContactForm';
 
 export default function Home() {
-  return (
-    <div className="mwi-split-layout">
-        <div className="mwi-full-layout mwi-split-dark-container">
-            <div className="mwi-full-content mwi-dark-content">
-                <header>
-                    <img src="images/Logo.png" className="mwi-logo" alt="Midwestern Interactive" />
-                    <div className="mwi-navigation" >
-                        <a href="/" className="mwi-navigation-condensed">home</a>
+    function contactFormHandler(contactFormData) {
+        fetch('https://tech-challenge-node-next-eglt2.ondigitalocean.app/contacts'),
+        {
+            method: 'POST',
+            body: JSON.stringify(contactFormData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    };
+
+    return (
+        <div className="mwi-split-layout">
+            <div className="mwi-full-layout mwi-split-dark-container">
+                <div className="mwi-full-content mwi-dark-content">
+                    <header>
+                        <img src="images/Logo.png" className="mwi-logo" alt="Midwestern Interactive" />
+                        <div className="mwi-navigation" >
+                            <a href="/" className="mwi-navigation-condensed">home</a>
+                        </div>
+                    </header>
+                    <div className="mwi-copy-section">
+                        <h1 className="mwi-conditional-space">
+                            <span className="mwi-underline">Heading</span> One
+                        </h1>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do dos eiusmod tempor incididunt ut labore et trace 
+                            dolore magna aliqua.
+                        </p>
+                        <p>
+                            Proin sagittis nisl rhoncus mattis rhoncus. at augue eget
+                            arcu dictum varius duis at consectetur lorem. 
+                        </p>
                     </div>
-                </header>
-                <div className="mwi-copy-section">
-                    <h1 className="mwi-conditional-space">
-                        <span className="mwi-underline">Heading</span> One
-                    </h1>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do dos eiusmod tempor incididunt ut labore et trace 
-                        dolore magna aliqua.
-                    </p>
-                    <p>
-                        Proin sagittis nisl rhoncus mattis rhoncus. at augue eget
-                        arcu dictum varius duis at consectetur lorem. 
-                    </p>
+                </div>
+            </div>
+                
+            <div className="mwi-split-light-container">
+                <div className="mwi-light-content">
+                    <header className="mwi-navigation-expanded">
+                        <span></span>
+                        <div className="mwi-navigation">
+                            <a href="/">home</a>
+                        </div>
+                    </header>
+                    <ContactForm onAddContact={contactFormHandler} />
                 </div>
             </div>
         </div>
-            
-        <div className="mwi-split-light-container">
-            <div className="mwi-light-content">
-                <header className="mwi-navigation-expanded">
-                    <span></span>
-                    <div className="mwi-navigation">
-                        <a href="/">home</a>
-                    </div>
-                </header>
-                <ContactForm />
-            </div>
-        </div>
-    </div>
-  )
+    )
 }
